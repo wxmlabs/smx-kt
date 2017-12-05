b86427f70057d8073cb28ba16224a1b0f12eca0e
Shining Wang <shiningwang@vshining.com>
2017-12-06 00:07:15 +0800

    为SMxProvider提升临时访问权限
    使用AccessController.doPrivileged为SMxProvider提升JVM的临时访问权限

--- smx-jvm/src/main/kotlin/wxmlabs/security/smx/SMxProvider.kt
+++ smx-jvm/src/main/kotlin/wxmlabs/security/smx/SMxProvider.kt
@@ -1,28 +1,50 @@
 package wxmlabs.security.smx
 
+import java.security.AccessController
+import java.security.PrivilegedAction
 import java.security.Provider
 import java.security.Security
 
-class SMxProvider() : Provider(name, version, info) {
+class SMxProvider : Provider(NAME, version, info) {
     init {
         registerSM3MessageDigest()
     }
 
     private fun registerSM3MessageDigest() {
-        put("MessageDigest.SM3",
-                wxmlabs.security.smx.SM3MessageDigest::class.qualifiedName)
-        put("Alg.Alias.MessageDigest.${SMxOID.SM3}", "SM3")
+        registerService("MessageDigest", "SM3", SM3MessageDigest::class.java.name, SMxOID.SM3)
+    }
+
+    /**
+     * @param service   服务名 如：Signature, MessageDigest, Cipher等
+     * @param algorithm 算法名 如：SM3withSM2, SM3, SM4等
+     * @param instance  算法实现类 如："wxmlabs.security.smx.SM3MessageDigest"
+     * @param aliases   算法别名 如：SM3算法的OID "1.2.156.10197.1.401"
+     */
+    private fun registerService(service: String, algorithm: String, instance: String, vararg aliases: String) {
+        AccessController.doPrivileged(PrivilegedAction<Void> {
+            val algorithmKey = String.format("%s.%s", service, algorithm)
+            put(algorithmKey, instance)
+            if (aliases.isNotEmpty()) {
+                aliases
+                    .map { alias -> "Alg.Alias.$service.$alias" /* aliasKey:String */ }
+                    .forEach { aliasKey -> put(aliasKey, algorithm) }
+            }
+            null
+        })
     }
 
     companion object {
-        private val name = "SMx"
+        const val NAME = "SMx"
         private val version = 0.1
-        private val info = "The cryptography provider for business of China."
+        private val info = "SMx Provider, the cryptography smx for business of China (implements SM3, SM4, SM2)."
 
+        @JvmField
         val INSTANCE = SMxProvider()
-        fun register(position: Int = 0) {
-            if (Security.getProvider(name) == null) {
-                Security.insertProviderAt(INSTANCE, position)
+
+        @JvmStatic
+        fun register() {
+            if (Security.getProvider(NAME) == null) {
+                Security.addProvider(INSTANCE)
             }
         }
     }
